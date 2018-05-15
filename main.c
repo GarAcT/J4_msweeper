b985d2b3ab0623181f4d1f45d415133a4e392876
GarAcT <shot.kit.garact@gmail.com>
2018-05-15 16:58:37 +0900

    [add] マスを開く処理
    またそれに伴う修正&整形

--- main.c
+++ main.c
@@ -13,12 +13,23 @@ void shuffle(int *stage);
 void show_map(int *stage);
 
 int  search_M(int *stage, int x, int y);
+int  open_cell(int *stage, int x, int y);
 
 int main(void)
 {
   int stage[HEIGHT*WIDTH];
 
+  int i,j;
+
   init(stage);
+
+  for(i = 0; i < HEIGHT; i++){
+    for(j = 0; j < WIDTH; j++){
+      open_cell(stage, j, i);
+    }
+  }
+
+  show_map(stage);
 }
 
 void init(int *stage)
@@ -56,17 +67,26 @@ void show_map(int *stage)
 
   for(i = 0; i < HEIGHT*WIDTH; i++){
     if(i%WIDTH == 0) puts("");
-    if(stage[i]%MASK) printf("M");
-    else {
+
+    if(stage[i] >= MASK){
+      printf("#");
+    }else if(stage[i] == 1){
+      printf("M");
+    }else if(stage[i] == 0){
       risk = search_M(stage,i%WIDTH,i/WIDTH);  
-      if(risk) printf("%d",risk);
-      else printf("-");
+      if(risk){
+        printf("%d",risk);
+      }else{
+        printf("-");
+      }
     }
   }
+
   if(i%WIDTH == 0) puts("");
 }
 
-int search_M(int *stage, int x, int y){
+int search_M(int *stage, int x, int y)
+{
   int i,j;
   int ret = 0;
 
@@ -77,9 +97,16 @@ int search_M(int *stage, int x, int y){
     if(y+i < 0 || y+i >= HEIGHT) continue;
     for(j=0; j<3; j++){
       if(x+j < 0 || x+j >= WIDTH) continue;
-      ret += stage[WIDTH*(y+i) + x+j] - MASK;
+      ret += stage[WIDTH*(y+i) + x+j] % MASK;
     }
   }
 
   return ret;
+}
+
+int open_cell(int *stage, int x, int y)
+{
+  stage[WIDTH*y + x] -= MASK;
+
+  return stage[WIDTH*y + x];
 }
