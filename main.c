d7f76cb114a5ff03f0018b2a4a0ee951912ddb32
GarAcT <shot.kit.garact@gmail.com>
2018-05-29 13:37:21 +0900

    [change] search_Mをx,y座標で動かすのをやめた
    こればかりはx,yで動かしたほうが楽だったけど他は全部posだったから統一した

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void init(int *stage);
 void shuffle(int *stage);
 void show_map(int *stage);
 
-int  search_M(int *stage, int x, int y);
+int  search_M(int *stage, int pos);
 int  open_cell(int *stage, int pos);
 int  input();
 
@@ -75,7 +75,7 @@ void show_map(int *stage)
     }else if(stage[i] == 1){
       printf("M");
     }else if(stage[i] == 0){
-      risk = search_M(stage,i%WIDTH,i/WIDTH);  
+      risk = search_M(stage,i);
       if(risk){
         printf("%d",risk);
       }else{
@@ -87,10 +87,14 @@ void show_map(int *stage)
   if(i%WIDTH == 0) puts("");
 }
 
-int search_M(int *stage, int x, int y)
+int search_M(int *stage, int pos)
 {
   int i,j;
+  int x,y;
   int ret = 0;
+
+  x = pos % WIDTH;
+  y = pos / WIDTH;
 
   for(i=-1; i<=1; i++){
     if(y+i < 0 || y+i >= HEIGHT) continue;
